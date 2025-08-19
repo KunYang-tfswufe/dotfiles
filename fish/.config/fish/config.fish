326aae6d02a030084c1def7447871862cde41cc9
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2025-08-19 09:18:57 +0000

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,17 +3,5 @@ if status is-interactive
 end
 
 alias cat 'bat --paging=never'
-# ---- A function to gently remind about using zoxide ----
-function cd
-    # First, execute the real, built-in cd command with all arguments
-    builtin cd $argv
-
-    # Then, check if the cd command was successful
-    if test $status -eq 0
-        # If successful, print a small tip
-        echo -e "\e[34mTip: Next time, try using 'z' for a faster jump!\e[0m"
-    end
-end
-# ------------------------------------
 zoxide init fish | source
 set -gx VISUAL nvim
