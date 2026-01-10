00ff7899eb1546e91933d32dc57e2fe1d5a30523
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-10 15:26:28 +0800

    update

--- scripts/.local/bin/setup.sh
+++ scripts/.local/bin/setup.sh
@@ -14,7 +14,7 @@ sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 
 # basic
-sudo dnf -y install wl-clipboard grim slurp jq dunst fzf stow ripgrep oathtool pass pass-otp gnupg
+sudo dnf -y install wl-clipboard wf-recorder grim slurp jq dunst fzf stow ripgrep oathtool pass pass-otp gnupg
 
 # sshd
 sudo dnf -y install openssh-server && sudo systemctl enable --now sshd
