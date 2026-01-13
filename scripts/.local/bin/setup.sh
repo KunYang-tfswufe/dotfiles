68a2df72888fe8d401d053ee73b6293cf64614d1
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-13 10:55:18 -0500

    update

--- scripts/.local/bin/setup.sh
+++ scripts/.local/bin/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # basic-debian
-sudo apt -y install nodejs npm hx fzf pass pass-otp oathtool stow gnupg ripgrep rclone gh mpv yt-dlp 7zip starship rust-eza tigervnc
+sudo apt -y install nodejs npm hx fzf pass pass-otp oathtool stow gnupg ripgrep rclone gh mpv yt-dlp 7zip starship
+
+# eza
+sudo apt -y eza
+
+# tigervnc
+sudo apt -y install tigervnc
 
 # update && upgrade
 sudo apt -y update && sudo apt -y upgrade
