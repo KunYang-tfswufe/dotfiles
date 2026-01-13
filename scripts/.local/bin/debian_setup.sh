dc985baed25de57c61160a6d8d026e6d70c3d4a0
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-13 02:52:25 -0500

    update

--- scripts/.local/bin/debian_setup.sh
+++ scripts/.local/bin/debian_setup.sh
@@ -13,3 +13,9 @@ sudo apt -y update && sudo apt -y upgrade
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
 	&& sudo apt install gh -y
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# eza
+cargo install eza
