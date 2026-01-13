6028b1ee4bc0828927a80686a83880471c52b736
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-13 08:50:15 -0500

    update

--- scripts/.local/bin/debian_setup.sh
+++ scripts/.local/bin/debian_setup.sh
@@ -39,10 +39,6 @@ cargo install bottom --locked
 # ripgrep
 cargo install ripgrep
 
-# bat
-sudo apt -y install bat
-ln -s /usr/bin/batcat ~/.local/bin/bat
-
 # prismlauncher
 sudo wget https://prism-launcher-for-debian.github.io/repo/prismlauncher.gpg -O /usr/share/keyrings/prismlauncher-archive-keyring.gpg \
   && echo "deb [signed-by=/usr/share/keyrings/prismlauncher-archive-keyring.gpg] https://prism-launcher-for-debian.github.io/repo $(. /etc/os-release; echo "${UBUNTU_CODENAME:-${DEBIAN_CODENAME:-${VERSION_CODENAME}}}") main" | sudo tee /etc/apt/sources.list.d/prismlauncher.list \
