8328d8b21ec73599c685203c10614788c68b5e94
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-14 12:54:01 -0500

    update commit

--- scripts/.local/bin/setup.sh
+++ scripts/.local/bin/setup.sh
@@ -7,9 +7,9 @@ sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install nodejs wget npm hx fzf pass pass-otp oathtool stow gnupg ripgrep rclone gh mpv yt-dlp 7zip starship eza arp-scan sshfs jq dunst xclip ffmpeg maim kitty i3-wm i3status i3lock
 
 # termurin jdk
-wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-sudo apt install temurin-21-jdk
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update && sudo apt -y install temurin-21-jdk
 
 # broadcom # Restart Required
 sudo sed -i 's/ main / main contrib non-free /g' /etc/apt/sources.list
