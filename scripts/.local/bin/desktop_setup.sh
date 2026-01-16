ab3cb58f5d25f077a10e6ab21a0b414d698bda3e
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-16 16:27:37 -0500

    update

--- scripts/.local/bin/desktop_setup.sh
+++ scripts/.local/bin/desktop_setup.sh
@@ -1,1 +1,6 @@
-qwqwq
+#!/bin/bash
+
+sudo wget https://prism-launcher-for-debian.github.io/repo/prismlauncher.gpg -O /usr/share/keyrings/prismlauncher-archive-keyring.gpg \
+  && echo "deb [signed-by=/usr/share/keyrings/prismlauncher-archive-keyring.gpg] https://prism-launcher-for-debian.github.io/repo $(. /etc/os-release; echo "${UBUNTU_CODENAME:-${DEBIAN_CODENAME:-${VERSION_CODENAME}}}") main" | sudo tee /etc/apt/sources.list.d/prismlauncher.list \
+  && sudo apt update \
+  && sudo apt install prismlauncher
