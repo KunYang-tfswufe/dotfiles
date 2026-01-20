cc1075ed857c3a2012f71e062ab062b0a80ea3d8
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-20 05:25:49 -0500

    init commit

--- scripts/.local/bin/base_setup.sh
+++ scripts/.local/bin/base_setup.sh
@@ -6,10 +6,6 @@ sudo apt -y update && sudo apt -y upgrade
 # basic-debian
 sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven htop nodejs npm
 
-# nodejs npm fnm
-curl -fsSL https://fnm.vercel.app/install | bash
-source $HOME/.config/fish/conf.d/fnm.fish
-
 # fish
 sudo apt -y install fish && sudo chsh -s $(which fish) $USER
 
@@ -33,4 +29,3 @@ sudo npm install -g repomix
 
 # helix
 wget https://github.com/helix-editor/helix/releases/download/25.07.1/helix_25.7.1-1_amd64.deb && sudo apt -y install ./helix_25.7.1-1_amd64.deb && rm helix_25.7.1-1_amd64.deb
-
