1d1c60c79f54583a2851215bf1bd15ef577b34b1
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-20 08:27:22 -0500

    update

--- scripts/.local/bin/base_setup.sh
+++ scripts/.local/bin/base_setup.sh
@@ -4,7 +4,7 @@
 sudo apt -y update && sudo apt -y upgrade
 
 # basic-debian
-sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven htop nodejs npm bat
+sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven htop nodejs npm bat gh
 
 # fish
 sudo apt -y install fish && sudo chsh -s $(which fish) $USER
