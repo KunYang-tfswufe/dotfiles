e32d715d0ad9902f6e0c2a925d2350f3aabb0d28
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-20 04:43:40 -0500

    update

--- scripts/.local/bin/base_setup.sh
+++ scripts/.local/bin/base_setup.sh
@@ -4,7 +4,7 @@
 sudo apt -y update && sudo apt -y upgrade
 
 # basic-debian
-sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk
+sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven
 
 # nodejs npm fnm
 curl -fsSL https://fnm.vercel.app/install | bash
