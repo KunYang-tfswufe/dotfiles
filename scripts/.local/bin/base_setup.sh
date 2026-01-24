5c47e0ce50b4aab7527309e1c352b441a98e30be
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-24 05:42:43 -0500

    update

--- scripts/.local/bin/base_setup.sh
+++ scripts/.local/bin/base_setup.sh
@@ -4,7 +4,10 @@
 sudo apt -y update && sudo apt -y upgrade
 
 # basic-debian
-sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven htop nodejs npm bat gh
+sudo apt -y install curl wget unzip picom fzf tmux pass pass-otp oathtool stow gnupg ripgrep rclone mpv yt-dlp 7zip starship eza arp-scan sshfs jq pandoc ffmpeg openjdk-21-jdk maven htop nodejs npm bat gh pipx
+
+# trafilatura
+pipx install trafilatura
 
 # fish
 sudo apt -y install fish && sudo chsh -s $(which fish) $USER
