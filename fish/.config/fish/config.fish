430596f7e04284a04eeedc04ea874a4bca50d6c1
KunYang-tfswufe <42212346@stu.tfswufe.edu.cn>
2026-01-29 17:39:22 -0500

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,7 +18,6 @@ if status is-interactive
     set fish_cursor_visual block
 
     bind -M default d delete-char
-    bind -M visual d kill-selection
 
     fzf_key_bindings
     bind \co 'set -l cmd (command cat ~/dotfiles/docs/commands.txt | fzf --reverse); if test -n "$cmd"; commandline -r -- $cmd; end; commandline -f repaint'
